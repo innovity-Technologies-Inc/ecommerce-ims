7789259a52c1911425c3b3712b8e46b2fffc100d
Daiyan <daiyan1091@gmail.com>
2026-03-09 10:12:33 +0600

    Admin Login blade UI Refinement

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,11 +10,11 @@
                                 <div class="auth-logo mb-4">
                                     @php($gs = \App\HelperClass::generalSettings())
                                     <a href="{{route('admin.login')}}" class="logo-dark">
-                                        <img src="{{ $gs->dark_logo ? asset('storage/'.$gs->dark_logo) : asset('admin/assets/images/logo-dark.png') }}" height="24" alt="logo dark">
+                                        <img src="{{ $gs->light_logo ? asset('storage/'.$gs->light_logo) : asset('admin/assets/images/logo-light.png') }}" height="50" alt="logo dark">
                                     </a>
 
                                     <a href="{{route('admin.login')}}" class="logo-light">
-                                        <img src="{{ $gs->light_logo ? asset('storage/'.$gs->light_logo) : asset('admin/assets/images/logo-light.png') }}" height="24" alt="logo light">
+                                        <img src="{{ $gs->light_logo ? asset('storage/'.$gs->light_logo) : asset('admin/assets/images/logo-light.png') }}" height="50" alt="logo light">
                                     </a>
                                 </div>
 
