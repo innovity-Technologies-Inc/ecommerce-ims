5c660592d2c4436450d81bdc0c5abb642cde3d79
Daiyan <daiyan1091@gmail.com>
2026-03-14 12:34:06 +0600

    feat: add SocialLoginSettingRequest for validation

---
+++ app/Http/Requests/SocialLoginSettingRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SocialLoginSettingRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'google_client_id' => ['nullable', 'string', 'max:255'],
+            'google_client_secret' => ['nullable', 'string', 'max:255'],
+            'google_redirect_url' => ['nullable', 'string', 'max:255'],
+            'google_status' => ['nullable', 'boolean'],
+        ];
+    }
+}
