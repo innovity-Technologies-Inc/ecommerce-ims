ba901f488aa41e1683c9963ce2ef79ded10e8493
Md Daiyan Uddin Mozumder <daiyan1091@gmail.com>
2026-07-30 16:35:51 +0600

    fix: restore header_account_area classes to fix search icon styling (REQ-244)

--- resources/views/client/structure/partials/header.blade.php
+++ resources/views/client/structure/partials/header.blade.php
@@ -187,7 +187,7 @@
                     </div>
                     <!--Main Navigation End -->
                     <!--Header Bottom Account Start -->
-                    <div class="header_account_area m-0 align-items-center">
+                    <div class="header_account_area">
                         <!--Seach Area Start -->
                         <div class="header_account_list search_list">
                             <a href="javascript:void(0)"><i class="ion-ios-search-strong"></i></a>
